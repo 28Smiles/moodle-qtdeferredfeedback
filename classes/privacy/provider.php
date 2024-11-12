306fd0604908836b5cebd5cb06f043af47245eca
Leon Camus <leon.camus@tu-darmstadt.de>
2024-11-12 14:14:21 +0100

    fix: Namespace

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace qbehaviour_deferredfeedback\privacy;
+namespace qbehaviour_qtdeferredfeedback\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
